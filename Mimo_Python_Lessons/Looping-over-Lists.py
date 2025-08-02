1282ef3981df0e5afb20c0049b5fe4ed85fb2967
Rabdool <abdurrahmanabdulkabir06@gmail.com>
2025-08-02 14:34:12 +0100

    mimo-lessons

---
+++ Mimo_Python_Lessons/Looping-over-Lists.py
@@ -0,0 +1,44 @@
+final_score = [17, 22, 34, 13]
+
+for score in final_score:
+    print(score)
+
+artists = ["Chagall", "Lissitzky"]
+
+for artist in artists:
+    print(artist)
+    print("---")
+
+student_grades = ["A", "-A", "C"]
+for grade in student_grades:
+    print(grade)
+
+singers = ["Bowie", "Freddie"]
+
+for singer in singers:
+    print(singer)
+
+consoles = ["Playstation", "Xbox"]
+
+for console in consoles:
+    print(console)
+
+items = ["milk", "tomato", "apple"]
+
+for item in items:
+    print(item)
+
+supplies = ["pencil", "book"]
+
+for value in supplies:
+    print(value)
+
+data_points = [99, 99, 99, 99]
+
+for data in data_points:
+    print(data + 1)
+
+minutes_worked = [123, 100, 99, 67]
+
+for minutes in minutes_worked:
+    print(minutes - 60)
