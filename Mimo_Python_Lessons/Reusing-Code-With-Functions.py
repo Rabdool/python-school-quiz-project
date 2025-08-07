bc868d469baac557a6aa4de66cb382bd1c9e6e2a
Rabdool <abdurrahmanabdulkabir06@gmail.com>
2025-08-07 12:36:00 +0100

    Reusing code with functions

---
+++ Mimo_Python_Lessons/Reusing-Code-With-Functions.py
@@ -0,0 +1,31 @@
+def greet_user(): 
+    print("Good morning, Anna")
+    print("Welcome back")
+greet_user()
+
+def weather_update():
+    print("Weather update")
+    print("Sunny skies ahead")
+weather_update()
+
+def night_routine():
+    print("Lights off")
+    print("Alarm set")
+night_routine()
+
+def morning_routine():
+    print("Lights on")
+    print("Alarm off")
+morning_routine()
+
+def launch():
+    print("3")
+    print("2")
+    print("1")
+    print("Launch!")
+launch()
+
+def greet():
+    print("Good evening")
+greet()
+    
