08953baa0c17af1923ed62e7a11aa2555f2cb976
Rabdool <abdurrahmanabdulkabir06@gmail.com>
2025-08-27 13:10:33 +0100

    Functions and variable scope

---
+++ Mimo_Python_Lessons/Functions-and-variable-scope.py
@@ -0,0 +1,18 @@
+def add_bonus(salary):
+    bonus = 100
+    print(salary + bonus)
+
+add_bonus(1000)
+
+def add_bonus(salary):
+    bonus = 100
+    bonus = 200
+    print(salary + bonus)
+
+add_bonus(1900)
+
+def apply_discount(price):
+    discount = 20
+    return price - discount
+final_price = apply_discount(50)
+print(final_price)
